2886c09f997cfae0c4d0884c3f5a93e2336df262
kmurphy <kmurphy@etsy.com>
2014-03-27 17:10:51 +0000

    start on command api

--- index.php
+++ index.php
@@ -6,49 +6,50 @@ ini_set('memory_limit', -1);
 
 header('Content-Type: application/json');
 
-$path_parts = split('/', $_SERVER['PATH_INFO']);
+$path_parts = explode('/', $_SERVER['PATH_INFO']);
 
 
 $client = new LiveStatusClient('/var/nagios/var/rw/live');
 $client->pretty_print = true;
 
-$topic = $path_parts[1];
-$query = new LiveStatusQuery($topic);
+/*
+$commands = [
+    'acknowledege_problem'
+    'schedule_downtime',
+    'enable_notifications',
+    'disable_notifications'
+];
+*/
 
-foreach ($_GET  as $key => $val) {
-    switch ($key) {
-    case 'Columns':
-        $columns = split(',', $val);
-        $query->setColumns($columns);
-        break;
-    case 'Filter':
-        if (is_array($val)) {
-            foreach ($val as $subvalue) {
-                $query->addFilter($subvalue);
-            }
-        } else {
-            $query->addFilter($val);
-        }
-        break;
-    case 'Stats':
-        if (is_array($val)) {
-            foreach ($val as $subvalue) {
-                $query->addStat($subvalue);
-            }
-        } else {
-            $query->addStat($val);
-        }
-        break;
-    default:
-        $query->setOption($key, $val);
-    }
-}
+$method = $path_parts[1];
+
 try {
-    echo $client->runQuery($query);
+    switch ($method) {
+
+    case 'acknowledge_problem':
+        $client->acknowledgeProblem($_POST);
+        echo json_encode(['OK']);
+        break;
+       
+    case 'schedule_downtime':
+        echo json_encode(['OK']);
+        break;
+
+    case 'enable_notifications':
+        echo json_encode(['OK']);
+        break;
+
+    case 'disable_notifications':
+        echo json_encode(['OK']);
+        break;
+
+    default:
+        echo $client->getQuery($method, $_GET);
+
+    }
 } catch (LiveStatusException $e) {
     http_response_code($e->getCode());
     echo $e->toJson();
 }
-
 
 ?>
--- livestatus_client.php
+++ livestatus_client.php
@@ -14,6 +14,7 @@ class LiveStatusClient
     {
         $this->socket_path = $socket_path;
         $this->socket = null;
+        $this->query = null;
         $this->pretty_print = false;
     }
 
@@ -21,7 +22,6 @@ class LiveStatusClient
     {
         $this->socket = stream_socket_client("unix://{$this->socket_path}");
     }
-
 
     private function _jsonOpts()
     {
@@ -78,7 +78,99 @@ class LiveStatusClient
 
         $query_string = $query->getQueryString();
         fwrite($this->socket, $query_string);
-        return $this->_fetchResponse();
+        $response = $this->_fetchResponse();
+        fclose($this->socket);
+    }
+
+    public function runCommand($command)
+    {
+        $this->_connect();
+        $command_string = $command->getCommandString();
+        fwrite($this->socket, $command_string);
+        fclose($this->socket);
+    }
+
+    function getQuery($method,$args=[]) 
+    {
+        $query = new LiveStatusQuery($method);
+
+        foreach ($args  as $key => $val) {
+            switch ($key) {
+            case 'Columns':
+                $columns = split(',', $val);
+                $query->setColumns($columns);
+                break;
+            case 'Filter':
+                if (is_array($val)) {
+                    foreach ($val as $subvalue) {
+                        $query->addFilter($subvalue);
+                    }
+                } else {
+                    $query->addFilter($val);
+                }
+                break;
+            case 'Stats':
+                if (is_array($val)) {
+                    foreach ($val as $subvalue) {
+                        $query->addStat($subvalue);
+                    }
+                } else {
+                    $query->addStat($val);
+                }
+                break;
+            default:
+                $query->setOption($key, $val);
+            }
+        }
+        return $client->runQuery($query);
+    }
+
+    private function _validateArgs($required, $args) {
+        foreach ($required as $field) {
+            if (!array_key_exists($field, $args)) {
+                throw new LiveStatusException(
+                    "Required field '$field' is missing", 
+                    400
+                );
+            }
+        }
+    }
+
+    public function acknowledgeProblem($args) {
+
+        $method = 'ACKNOWLEDGE_SVC_PROBLEM';
+        $required = [
+            'host_name',
+            'author',
+            'comment',
+        ];
+
+        $fields = [
+            'host_name' => '',
+            'service_description' => '',
+            'sticky'    => 1,
+            'notify'    => 1,
+            'author'    => '',
+            'comment'   => '',
+        ];
+
+
+        $this->_validateArgs($required, $args);
+
+        foreach ($fields as $field => $val) {
+            if (array_key_exists($field)) {
+                $fields[$field] = $args[$field];
+            }
+        }
+
+        if (!$fields['service_description']) {
+            unset($fields['service_description']);
+            $method = 'ACKNOWLEDGE_HOST_PROBLEM';
+        }
+
+        $cmd = new LiveStatusCommand( array_merge([$method], array_values($fields)) );
+        $this-runCommand($cmd);
+
     }
 
 }
@@ -140,3 +232,20 @@ class LiveStatusQuery
 
     }
 }
+
+class LiveStatusCommand
+{
+    function __construct($args=[])
+    {
+        $this->method = $args[0];
+        $this->args   = $args;
+    }
+
+    function getCommandString()
+    {
+        $command = "COMMAND ";
+        $command .= sprintf("[%d] ", time());
+        $command .= join($args, ';');
+    }
+}
+
